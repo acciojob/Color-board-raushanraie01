1eff03a739f8eb7e7994fb403f05aa4e7803a6a0
Raushan Kumar <113075873+raushanraie01@users.noreply.github.com>
2025-11-17 22:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
+const SQUARES = 800;
+
+// create 800 squares
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    // Hover effect
+    square.addEventListener("mouseover", () => setColor(square));
+
+    // Remove color after mouse leaves (smooth fade)
+    square.addEventListener("mouseleave", () => removeColor(square));
+
+    container.appendChild(square);
+}
+
+// set random color
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+}
+
+function removeColor(element) {
+    element.style.background = "#1d1d1d"; // default
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
